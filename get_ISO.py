0f10d61e67bbf9178975cec2be50d4b1dce43109
Avinash Verma <avinash.verma@niser.ac.in>
2023-07-25 19:30:40 +0530

    --allow-empty

--- get_ISO.py
+++ get_ISO.py
@@ -1,5 +1,6 @@
 # This file gets the list of all Isotopologues from the HITRAN database
 # and stores them in a file called "iso_list.txt"
+# Make sure pandas and lxml module are installed before using this script
 
 #####################
 import pandas as pd #
@@ -13,7 +14,9 @@ def main():
 
     data=pd.read_html(url,header=0)
 
-    iso = []
+    species_id = []
+    global_id = []
+    isotope_no = []
     qfile = []
     Formula = []
 
@@ -23,18 +26,20 @@ def main():
         for i in range(len(list)):
             qfile.append(list[i][7])
             Formula.append(list[i][2])
+            isotope_no.append(list[i][1])
+            global_id.append(str(j+1))
             index = int(list[i][1])
 
             if (index > 10):
                 index = symbols[(index%10)-1]
 
-            iso.append(str(j+1) + str(index))
+            species_id.append(str(j+1) + str(index))
 
     f = open("iso_list.txt","w")
-    print("%-4s %-8s %-16s %-16s" % ("No.","ISO_no","Molecule","Q_file"), file = f)
+    print("%-10s %-12s %-12s %-16s %-16s" % ("Global_id", "Isotope_id", "Species_id","Molecule_name","Q_file"), file = f)
 
-    for i in range(len(iso)):
-        print("%-4d %-8s %-16s %-16s" % (i+1,iso[i],Formula[i],qfile[i]), file = f)
+    for i in range(len(species_id)):
+        print("%-10s %-12s %-12s %-16s %-16s" % (global_id[i], isotope_no[i] ,species_id[i], Formula[i], qfile[i]), file = f)
     
     f.close()
     
